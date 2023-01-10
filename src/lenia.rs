f1345e3338f536217fdabb466f9ded81dfe02293
Birdbrain <admirethemakeroftime@gmail.com>
2023-01-10 17:49:15 +0100

    Working Lenia

--- src/lenia.rs
+++ src/lenia.rs
@@ -1,4 +1,4 @@
-use ndarray::{self, IxDyn, Axis, s, Slice};
+use ndarray::{self, IxDyn, Axis, s, Slice, Order};
 use num_complex::Complex;
 use crate::fft;
 
@@ -11,12 +11,11 @@ pub mod utils {
 
         /// functions
         // Creates the kernel base for a gaussian donut in 2d. 
-        // The peak is placed at radius/2 distance from center. 
+        // The peak is placed at radius/2 distance from center.
+        // stddev of ~1.0/3.35 gives a good kernel for standard Lenia.
         // This is one of the default kernels for 2d Lenia.
-        pub fn gaussian_donut_2d(diameter: usize) -> ndarray::ArrayD<f64> {
+        pub fn gaussian_donut_2d(diameter: usize, stddev: f64) -> ndarray::ArrayD<f64> {
             let radius = diameter as f64 / 2.0;
-            let distribution = Gaussian::new(diameter as f64 / 4.0, diameter as f64 / 12.0);
-            let scaler = 1.0 / distribution.density(diameter as f64 / 4.0);
             let mut out = ndarray::ArrayD::zeros(IxDyn(&[diameter, diameter]));
             let x0 = radius;
             let y0 = radius;
@@ -26,7 +25,7 @@ pub mod utils {
                     let y1 = j as f64;
                     let dist = ((x1-x0)*(x1-x0)+(y1-y0)*(y1-y0)).sqrt();
                     if dist <= radius { 
-                        out[[i, j]] = distribution.density(dist) * scaler; 
+                        out[[i, j]] = super::sample_normal(dist, diameter as f64 * 0.25, diameter as f64 * 0.25 * stddev);
                     }
                     else { 
                         out[[i, j]] = 0.0
@@ -35,104 +34,339 @@ pub mod utils {
             }
             out
         }
+
+        pub fn game_of_life() -> ndarray::ArrayD<f64> {
+            let mut out = ndarray::ArrayD::from_elem(vec![3 as usize, 3], 1.0);
+            out[[1, 1]] = 0.0;
+            out
+        }
     }
 
     pub mod growth_functions {
         pub fn standard_lenia(num: f64) -> f64 {
-            let x = num;
-            let b = 0.3;
-            let c = 0.03;
-            (2.0 * (-(((x - b) * (x - b))/(2.0 * 0.0009))).exp()) - 1.0
+            (2.0 * super::sample_normal(num, 0.15, 0.017)) - 1.0
         }
 
         pub fn game_of_life(num: f64) -> f64 {
-            if num < 0.222222222 || num >= 0.444444444 { return -1.0 }
-            else if num < 0.333333333 { return 0.0 }
-            else { return 1.0 }
+            let index = (num * 9.0).round() as usize;
+            if index == 2 { 0.0 }
+            else if index == 3 { 1.0 }
+            else {-1.0 }
         }
 
         pub fn constant(num: f64) -> f64 {
             1.0
         }
     }
+
+    pub mod initializations {
+        use rand::Rng;
+
+        fn generate_random_hypercube(shape: &[usize], scaler: f64, discrete: bool) -> ndarray::ArrayD<f64> {
+            let mut generator = rand::thread_rng();
+            ndarray::ArrayD::from_shape_simple_fn(shape, 
+                || {
+                    if discrete { (scaler * generator.gen::<f64>()).round() }
+                    else { scaler * generator.gen::<f64>() }
+                }
+            )
+        }
+
+        pub fn random_uniform(shape: &[usize], scaler: f64, discrete: bool) -> ndarray::ArrayD<f64> {
+            let mut generator = rand::thread_rng();
+            ndarray::ArrayD::from_shape_fn(shape, |_| {
+                if discrete { (scaler * generator.gen::<f64>()).round() }
+                    else { scaler * generator.gen::<f64>() }
+            })
+        }
+
+        pub fn random_gaussian_2d(shape: &[usize], stddev: f64, scaler: f64, discrete: bool) -> ndarray::ArrayD<f64> {
+            let mut generator = rand::thread_rng();
+            if shape.len() != 2 { 
+                panic!("Seeder \"random_gaussian_2d\" expects 2 dimensions but was given {}",shape.len()); 
+            }
+            let mut out = ndarray::ArrayD::from_shape_simple_fn(shape, 
+                || {
+                    generator.gen::<f64>()
+                }
+            );
+            let x0 = shape[0] as f64 / 2.0;
+            let y0 = shape[1] as f64 / 2.0;
+            for x in 0..shape[0] {
+                for y in 0..shape[1] {
+                    let x1 = x as f64;
+                    let y1 = y as f64;
+                    let dist = ((x1-x0)*(x1-x0)+(y1-y0)*(y1-y0)).sqrt();
+                    out[[x, y]] = scaler * out[[x, y]] * super::sample_normal(dist, 0.0, stddev);
+                    if discrete { out[[x, y]] = out[[x, y]].round(); }
+                }
+            }
+            out
+        }
+
+        pub fn random_hypercubic(shape: &[usize], radius: usize, scaler: f64, discrete: bool) -> ndarray::ArrayD<f64> {
+            let mut out = ndarray::ArrayD::from_elem(shape, 0.0);
+            let mut cube_dims: Vec<usize> = Vec::new();
+            for dim in shape {
+                if *dim < (radius * 2) { cube_dims.push(*dim); }
+                else { cube_dims.push(radius * 2); }
+            }
+            let cube = generate_random_hypercube(&cube_dims, scaler, discrete);
+            cube.assign_to(out.slice_each_axis_mut(
+                |a| ndarray::Slice {
+                    start: (a.len/2 - cube.shape()[a.axis.index()]/2) as isize,
+                    end: Some((
+                        a.len/2
+                        + cube.shape()[a.axis.index()]/2 
+                    ) as isize),
+                    step: 1
+                }
+            ));
+            out
+        }
+
+        pub fn random_hypercubic_patches(shape: &[usize], radius: usize, patches:usize, scaler:f64, discrete: bool) -> ndarray::ArrayD<f64> {
+            let mut generator = rand::thread_rng();
+            let mut buf: Vec<ndarray::ArrayD<f64>> = Vec::new();
+            let mut cube_dims: Vec<usize> = Vec:: new();
+            for dim in shape {
+                if *dim < (radius * 2) {
+                    cube_dims.push(*dim - 2);
+                }
+                else {
+                    cube_dims.push(radius);
+                }
+            }
+            for _ in 0..patches {
+                let mut patch = ndarray::ArrayD::from_elem(shape, 0.0);
+                generate_random_hypercube(&cube_dims, scaler, discrete).assign_to(patch.slice_each_axis_mut(
+                    |a| {
+                        let randindex = generator.gen_range(0..(a.len - cube_dims[a.axis.index()]));
+                        ndarray::Slice {
+                            start: randindex as isize,
+                            end: Some((randindex + cube_dims[a.axis.index()]) as isize),
+                            step: 1,
+                        }
+                    }
+                ));
+                buf.push(patch);
+            }
+            let mut out = ndarray::ArrayD::from_elem(shape, 0.0);
+            for i in (0..patches).rev() {
+                out.zip_mut_with(&buf[i], 
+                    |a, b| { 
+                        *a = (*a + *b).clamp(0.0, 1.0);
+                        if discrete { *a = a.round(); }
+                    }
+                )
+            }
+            out
+        }
+    }
+
+    // Samples the normal distribution where the peak (at x = mu) is 1.
+    // Do not use for gaussian probability density!
+    fn sample_normal(x: f64, mu: f64, stddev: f64) -> f64 {
+        (-(((x - mu) * (x - mu))/(2.0 * (stddev * stddev)))).exp()
+    }
 }
 
 pub struct Simulator<L: Lenia> {
     sim: L,
-    kernel: Kernel,
 }
 
 impl<L: Lenia> Simulator<L> {
     pub fn new(simulation_type: L) -> Self where L: Lenia {
         Simulator{
             sim: simulation_type,
-            kernel: Kernel::from(utils::kernels::gaussian_donut_2d(100), &[200, 200]),
         }
     }
 
-    pub fn iterate(&self) {
+    pub fn set_kernel(&mut self, kernel: ndarray::ArrayD<f64>, convolution_channel: usize) {
+        self.sim.set_kernel(kernel, convolution_channel);
+    }
+
+    pub fn set_growth_function(&mut self, f: fn(f64) -> f64, convolution_channel: usize) {
+        self.sim.set_growth(f, convolution_channel);
+    }
+
+    pub fn set_dt(&mut self, dt: f64) {
+        self.sim.set_dt(dt);
+    }
+
+    pub fn iterate(&mut self) {
         self.sim.iterate();
+    }
+
+    pub fn fill_channel(&mut self, data: &ndarray::ArrayD<f64>, channel: usize) {
+        self.sim.set_channel(data, channel);
+    }
+
+    pub fn get_frame(&self, channel: usize, display_axes: &[usize; 2], dimensions: &[usize]) -> ndarray::Array2<f64> {
+        let data_ref = self.sim.get_data_asref(channel);
+        return data_ref.slice_each_axis(
+            |a|{
+                if a.axis.index() == display_axes[0] || a.axis.index() == display_axes[1] {
+                    return Slice {
+                        start: 0,
+                        end: None,
+                        step: 1,
+                    }
+                }
+                else {
+                    return Slice {
+                        start: dimensions[a.axis.index()] as isize,
+                        end: Some(dimensions[a.axis.index() + 1] as isize),
+                        step: 1,
+                    }
+                }
+            }
+        ).to_shape(
+            ((
+                data_ref.shape()[display_axes[0]],
+                data_ref.shape()[display_axes[1]]
+            ), Order::RowMajor)
+        ).unwrap()
+        .mapv(|el| { el.re });
     }
 }
 
 pub trait Lenia {
-    fn add_channels(&self) {}
-    fn add_affine_channel(&self) {}
-    fn set_kernel(&self);
-    fn set_growth(&self);
-    fn set_weights(&self);
-    fn set_dt(&self);
-    fn iterate(&self);
+    fn initialize_channels(&mut self, shape: &[usize], num_channels: usize) {}
+    fn add_conv_channels(&mut self, num_conv_channels: usize) {}
+    fn set_kernel(&mut self, kernel: ndarray::ArrayD<f64>, conv_channel: usize) {}
+    fn set_growth(&mut self, f: fn(f64) -> f64, conv_channel: usize) {}
+    fn set_weights(&mut self, new_weights: &[f64], conv_channel: usize) {}
+    fn set_dt(&mut self, new_dt: f64) {}
+    fn set_channel(&mut self, data: &ndarray::ArrayD<f64>, channel: usize) {}
+    fn get_data(&self, channel: usize) -> ndarray::ArrayD<f64>;
+    fn get_data_asref(&self, channel: usize) -> &ndarray::ArrayD<Complex<f64>>;
+    fn iterate(&mut self);
 }
 
 pub struct StandardLenia {
+    dt: f64,
+    channel: Vec<Channel>,
+    buffer: Vec<ndarray::ArrayD<Complex<f64>>>,
+    conv_channel: Vec<ConvolutionChannel>,
 
 }
 
 impl StandardLenia {
-    pub fn new() -> Self {
-        StandardLenia{}
+    pub fn new(dimensions: &[usize]) -> Self {
+        if dimensions.len() < 2 || dimensions.len() > 2 { 
+            panic!("Expected 2 dimensions for Standard Lenia! Found {}", dimensions.len()); 
+        }
+        let mut proper_dims: Vec<usize> = Vec::new();
+        for (i, dim) in dimensions.iter().enumerate() {
+            if *dim < 25 {
+                println!("Dimension {} is extremely small ({} pixels). Resized dimension to 25 pixels.", i, *dim);
+                proper_dims.push(25);
+            }
+            else {
+                proper_dims.push(*dim);
+            }
+        }
+        let kernel = Kernel::from(utils::kernels::gaussian_donut_2d(61, 1.0/3.35), &proper_dims);
+        let conv_field = ndarray::ArrayD::from_elem(proper_dims, Complex::new(0.0, 0.0));
+        let conv_channel = ConvolutionChannel {
+            input_channel: 0,
+            input_buffer: conv_field.clone(),
+            kernel: kernel,
+            field: conv_field,
+            growth: utils::growth_functions::standard_lenia,
+        };
+
+        let channel = Channel {
+            field: conv_channel.field.clone(),
+            inverse_weight_sums: vec![1.0],
+        };
+        
+        StandardLenia{
+            dt: 0.1,
+            channel: vec![channel],
+            buffer: vec![conv_channel.field.clone()],
+            conv_channel: vec![conv_channel],
+        }
     }
 }
 
 impl Lenia for StandardLenia {
-    fn iterate(&self) {
-        
+    fn iterate(&mut self) {
+        self.conv_channel[0].input_buffer.zip_mut_with(&self.channel[0].field, 
+            |a, b| {
+                a.re = b.re;
+                a.im = 0.0;
+            }
+        );
+        fft::fftnd(&mut self.conv_channel[0].input_buffer, &mut self.buffer[0], &[0, 1]);
+        self.buffer[0].zip_mut_with(&self.conv_channel[0].kernel.transformed, 
+            |a, b| {    // Complex multiplication without cloning
+                let ac = a.re * b.re;
+                let bd = a.im * b.im;
+                let real = ac - bd;
+                a.im = ((a.re + a.im) * (b.re + b.im)) - real;
+                a.re = real;
+            }
+        );
+        fft::ifftnd(&mut self.buffer[0], &mut self.conv_channel[0].field, &[1, 0]);
+        self.channel[0].field.zip_mut_with(&self.conv_channel[0].field, 
+            |a, b| {
+                a.re = (a.re + ((self.conv_channel[0].growth)(b.re) * self.dt)).clamp(0.0, 1.0);
+                a.im = 0.0;
+            }
+        );
     }
 
-    fn set_kernel(&self) {
-
+    fn set_kernel(&mut self, kernel: ndarray::ArrayD<f64>, conv_channel: usize) {
+        self.conv_channel[0].kernel = Kernel::from(kernel, self.channel[0].field.shape());
     }
 
-    fn set_growth(&self) {
-        
+    fn set_growth(&mut self, f: fn(f64) -> f64, conv_channel: usize) {
+        self.conv_channel[0].growth = f;
     }
 
-    fn set_weights(&self) {
-        
+    fn set_dt(&mut self, new_dt: f64) {
+        self.dt = new_dt;
     }
 
-    fn set_dt(&self) {
-        
+    fn get_data(&self, channel: usize) -> ndarray::ArrayD<f64> {
+        self.channel[0].field.mapv(|a| { a.re })
+    }
+
+    fn get_data_asref(&self, channel: usize) -> &ndarray::ArrayD<Complex<f64>> {
+        &self.channel[0].field
+    }
+
+    fn set_channel(&mut self, data: &ndarray::ArrayD<f64>, channel: usize) {
+        self.channel[0].field.zip_mut_with(data, 
+            |a, b| {
+                a.re = *b;
+                a.im = 0.0;
+            }
+        );
     }
 }
 
 
 struct Channel {
-    field: ndarray::ArrayD<Complex<f64>>,
+    pub field: ndarray::ArrayD<Complex<f64>>,
+    pub inverse_weight_sums: Vec<f64>,
 }
 
 struct ConvolutionChannel {
-    field: ndarray::ArrayD<Complex<f64>>,
-    kernel: Kernel,
-    growth: fn(f64, &[f64]) -> f64,
+    pub input_channel: usize,
+    pub input_buffer: ndarray::ArrayD<Complex<f64>>,
+    pub field: ndarray::ArrayD<Complex<f64>>,
+    pub kernel: Kernel,
+    pub growth: fn(f64) -> f64,
 }
 
 
 struct Kernel {
-    base: ndarray::ArrayD<f64>,
-    normalized: ndarray::ArrayD<f64>,
-    transformed: ndarray::ArrayD<Complex<f64>>,
+    pub base: ndarray::ArrayD<f64>,
+    pub normalized: ndarray::ArrayD<f64>,
+    pub transformed: ndarray::ArrayD<Complex<f64>>,
 }
 
 impl Kernel {
@@ -145,14 +379,14 @@ impl Kernel {
         // Check for coherence in dimensionality and that the kernel is not
         // larger than the channel it is used to convolve with.
         if normalized_kernel.shape().len() != shifted_and_fft.shape().len() { 
-            panic!{"Supplied kernel dimensionality does not match the supplied channel dimensionality!
+            panic!("Supplied kernel dimensionality does not match the supplied channel dimensionality!
                 \nkernel: {} dimensional vs. channel: {} dimensional",
                 normalized_kernel.shape().len(), shifted_and_fft.shape().len()
-            }; 
+            ); 
         }
         for (i, dim) in normalized_kernel.shape().iter().enumerate() {
             if *dim > shifted_and_fft.shape()[i] { 
-                panic!{"Supplied kernel is larger than the channel it acts on in axis {}!", i} 
+                panic!("Supplied kernel is larger than the channel it acts on in axis {}!", i);
             }
         }
 
@@ -166,11 +400,15 @@ impl Kernel {
         let mut shifted = ndarray::ArrayD::from_elem(channel_shape, 0.0);
 
         normalized_kernel.assign_to(shifted.slice_each_axis_mut(
-            |a| Slice::new(
-                (a.len/2 - normalized_kernel.shape()[a.axis.index()]/2) as isize,
-                Some((a.len/2 + normalized_kernel.shape()[a.axis.index()]/2) as isize),
-                1
-            )
+            |a| Slice {
+                start: (a.len/2 - normalized_kernel.shape()[a.axis.index()]/2) as isize,
+                end: Some((
+                    a.len/2
+                    + normalized_kernel.shape()[a.axis.index()]/2 
+                    + normalized_kernel.shape()[a.axis.index()]%2
+                ) as isize),
+                step: 1
+            }
         ));
         
         // Shift the kernel into the corner
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,28 @@ use ndarray::{self, IxDyn};
 use num_complex::Complex;
 use pixel_canvas::{Canvas, Color, input};
 
-const SIDE_LEN: usize = 200;
+const SIDE_LEN: usize = 150;
 const SCALE: usize = 4;
 
 
 fn main() {
     std::env::set_var("RUST_BACKTRACE", "1");
+    let inv_scale = 1.0 / SCALE as f64;
 
-    let lenia_simulator = lenia::Simulator::new(lenia::StandardLenia::new());
+    let mut lenia_simulator = lenia::Simulator::new(lenia::StandardLenia::new(&[SIDE_LEN, SIDE_LEN]));
+    lenia_simulator.fill_channel(
+        &lenia::utils::initializations::random_hypercubic(&[SIDE_LEN, SIDE_LEN], 33, 0.4, false), 
+        0
+    );
+    //lenia_simulator.set_dt(0.05);
+    /*let mut lenia_simulator = lenia::Simulator::new(lenia::StandardLenia::new(&[SIDE_LEN, SIDE_LEN]));
+    lenia_simulator.set_growth_function(lenia::utils::growth_functions::game_of_life, 0);
+    lenia_simulator.set_kernel(lenia::utils::kernels::game_of_life(), 0);
+    lenia_simulator.set_dt(1.0);
+    lenia_simulator.fill_channel(
+        &lenia::utils::initializations::random_cube(&[SIDE_LEN, SIDE_LEN], 8, true), 
+        0,
+    );*/
 
     let canvas = Canvas::new(SIDE_LEN * SCALE, SIDE_LEN * SCALE)
         .title("Lenia")
@@ -23,20 +37,29 @@ fn main() {
 
         match keyboardstate.character {
             'r' => {
-
+                /*lenia_simulator.fill_channel(
+                    &lenia::utils::initializations::random_gaussian_2d(&[SIDE_LEN, SIDE_LEN], 30.0, true), 
+                    0
+                );*/
+                lenia_simulator.fill_channel(
+                    &lenia::utils::initializations::random_hypercubic_patches(&[SIDE_LEN, SIDE_LEN], SIDE_LEN / 6, 15, 0.4, false), 
+                    0
+                );
             }
             _ => {}
         }
         keyboardstate.character = '\0';
 
+        let frame = lenia_simulator.get_frame(0, &[0, 1], &[0, 0]);
+
         let width = image.width() as usize;
         for (y, row) in image.chunks_mut(width).enumerate() {
             for (x, pixel) in row.iter_mut().enumerate() {
-                pixel.r = ((lenia::utils::growth_functions::standard_lenia(x as f64 / (SIDE_LEN * SCALE) as f64) + 1.0) * 120.0) as u8;
+                pixel.r = (frame[[(x as f64 * inv_scale) as usize, (y as f64 * inv_scale) as usize]] * 255.0) as u8;
                 pixel.g = pixel.r;
                 pixel.b = pixel.r;
-                
             }
         }
+        lenia_simulator.iterate();
     });
 }
